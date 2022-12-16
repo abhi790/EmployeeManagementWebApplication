c9d386188b7ff2bec42605bed456562513ed2b9e
VMAdmin <abhimanyu.roy@itcinfotech.com>
2022-12-16 13:42:37 +0000

    added Controller in the application

---
+++ EmployeeManagement/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmployeeManagement
+{
+    public class Startup
+    {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            //Below code will show proper issue to be resolve by developer in order to work app properly
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //Serve the static files first
+            app.UseStaticFiles();
+            //then process mvc if any
+            app.UseMvcWithDefaultRoute();
+            
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World");
+            });
+            
+
+
+        }
+    }
+}
